5748d5a8b968a5cd9f88eaaa472ec7683de01a80
Jouperse Cabaluna <User@USER-PC>
2018-01-29 22:18:05 +0800

    read and add working

--- IPFSTest/IPFSTest/Form1.Designer.cs
+++ IPFSTest/IPFSTest/Form1.Designer.cs
@@ -32,28 +32,32 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 41);
+            this.button1.Location = new System.Drawing.Point(12, 41);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Add And Read";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(127, 83);
+            this.textBox1.Location = new System.Drawing.Point(12, 85);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(127, 122);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 122);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(100, 96);
             this.richTextBox1.TabIndex = 2;
@@ -61,17 +65,57 @@
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(233, 122);
+            this.richTextBox2.Location = new System.Drawing.Point(118, 122);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.Size = new System.Drawing.Size(100, 96);
             this.richTextBox2.TabIndex = 3;
             this.richTextBox2.Text = "";
             // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(224, 122);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Remove File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Start Ipfs";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(330, 122);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox4.TabIndex = 7;
+            this.richTextBox4.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 262);
+            this.ClientSize = new System.Drawing.Size(461, 262);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
@@ -89,6 +133,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
     }
 }
 
--- IPFSTest/IPFSTest/Form1.cs
+++ IPFSTest/IPFSTest/Form1.cs
@@ -11,10 +11,11 @@ using System.Windows.Forms;
 //using Ipfs;
 using Ipfs.Api;
 //using Ipfs.CoreApi;
-using Ipfs.Commands;
+//using Ipfs.Commands;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
+//using Ipfs;
 
 namespace IPFSTest
 {
@@ -24,11 +25,67 @@ namespace IPFSTest
         {
             InitializeComponent();
         }
-
-        private async void button1_Click(object sender, EventArgs e)
+        public string x;
+        public async void button1_Click(object sender, EventArgs e)
         {
-            //CancellationToken cancel;
+            CancellationToken cancel;
 
+            var ipfs = new IpfsClient();
+            const string filename = "/ipfs/QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv/readme";
+            string text = await ipfs.FileSystem.ReadAllTextAsync(filename);
+            richTextBox1.Text = text;
+            
+            string fileName2 = @"C:\go-ipfs\hello.txt";
+            File.WriteAllText(fileName2, "hello world 2");
+            Stream stream = File.Open(fileName2, FileMode.Open);
+            
+            var aaa = await ipfs.FileSystem.AddAsync(stream, "trist");
+            x = aaa.Id.Hash.ToString();
+            string x2 = aaa.ToLink("trist").Name.ToString();
+            richTextBox2.Text = await ipfs.FileSystem.ReadAllTextAsync(aaa.Id.Hash.ToString());
+            richTextBox3.Text = "/" + aaa.Id.GetHashCode();
+           
+            /**
+            using (var ipfs = new Ipfs.IpfsClient())
+            {
+                //Name of the file to add
+                string fileName = @"C:\go-ipfs\hello.txt";
+
+                //Wrap our stream in an IpfsStream, so it has a file name.
+                Ipfs.IpfsStream inputStream = new Ipfs.IpfsStream(fileName, File.OpenRead(fileName));
+
+                Ipfs.MerkleNode node = await ipfs.Add(inputStream);
+
+                Stream outputStream = await ipfs.Cat(node.Hash.ToString());
+
+                using (StreamReader sr = new StreamReader(outputStream))
+                {
+                    string contents = sr.ReadToEnd();
+                    richTextBox2.Text = contents;
+                    //Console.WriteLine(contents); //Contents of test.txt are printed here!
+                }
+            }*/
+
+        }
+
+        public async void button2_Click(object sender, EventArgs e)
+        {
+            CancellationToken cancel;
+            var ipfs = new IpfsClient();
+          
+            var json = await ipfs.DoCommandAsync("files/rm", cancel, "/" + x);
+
+            richTextBox3.Text = "/" + aaa.Id.GetHashCode();
+            //richTextBox4.Text = x2;
+            var x3 = await ipfs.Key.RemoveAsync(aaa.Id.GetHashCode().ToString());
+            //var bbb = await ipfs.FileSystem.ListFileAsync(aaa.Id.Hash.ToString());
+            //richTextBox3.Text = await ipfs.FileSystem.ReadAllTextAsync(aaa.Id.Hash.ToString());
+            //richTextBox3.Text = bbb.ToString();         
+            richTextBox4.Text = await ipfs.FileSystem.ReadAllTextAsync(aaa.Id.Hash.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
             //Process process = Process.Start("cmd.EXE");
             //ProcessStartInfo startInfo = new ProcessStartInfo();
             //startInfo.FileName = @"C:\go-ipfs\ipfs.EXE";
@@ -39,105 +96,12 @@ namespace IPFSTest
 
             //startInfo.Arguments = "ipfs daemon";
             //Process.Start(startInfo);
-            
-            var ipfs = new IpfsClient();
+
+
             //var b = await ipfs.DoCommandAsync("swarm/peers",cancel);
             //richTextBox2.Text = ipfs.Swarm.ToString();
             //var c = await ipfs.DoCommandAsync("daemon", cancel);
             //var a = await ipfs.FileSystem.ReadAllTextAsync("aa.txt");
-           
-            const string filename = "/ipfs/QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv/readme";
-            string text = await ipfs.FileSystem.ReadAllTextAsync(filename);
-            richTextBox1.Text = text;
-            /**
-            string fileName = @"C:\go-ipfs\hello.txt";
-            File.WriteAllText(fileName, "hello world 2");
-            var path = Path.GetFileName(fileName);
-           
-            var aaa = ipfs.FileSystem.AddFileAsync(path);
-            var txt2 = aaa.Result;
-            
-            richTextBox2.Text = txt2.ToString();
-            */
-
-            //string fileName2 = @"C:\go-ipfs\hello.txt";
-            //Stream stream = File.Open(fileName2, FileMode.Open);
-            //var aaa = await ipfs.FileSystem.AddAsync(stream, "ipfstest");
-            //richTextBox1.Text = await ipfs.FileSystem.ReadAllTextAsync(aaa.Id.Hash.ToString());
-
-            //var service = new IpfsService("https://ipfs.infura.io:5001");
-            //var node = service.AddFileAsync(@"C:\go-ipfs\hello.txt").Result;
-            //var hash = node.Hash.ToString();
-            //Process.Start("https://ipfs.infura.io/ipfs/" + hash);
-            //Console.ReadLine();
-
-            //const string filename = "QmXarR6rgkQ2fDSHjSY5nM2kuCXKYGViky5nohtwgF65Ec/about";
-            //string text = await ipfs.FileSystem.ReadAllTextAsync(filename);
-
-            /**
-            string fileName = @"C:\go-ipfs\testme.txt";
-            File.WriteAllText(fileName, "hello world 2");
-            var path = Path.GetFileName(fileName);
-            var ipfs2 = new IpfsClient();
-            var aaa = ipfs2.FileSystem.AddFileAsync(path);
-            var txt2 = aaa.Result;
-            Console.WriteLine(txt2);
-            textBox1.Text = txt2.ToString();
-            */
-            //var ipfs = new IpfsClient();
-            //read readme.txt 
-            //string filename1 = "QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv/about";
-            //string text1 = await ipfs.FileSystem.ReadAllTextAsync(filename1);
-            //richTextBox1.Text = text;
-            // add ipfs-test.txt in ipfs, then read the uploaded file using this file's ipfs address/hash 
-            string fileName2 = @"C:\go-ipfs\hello.txt";
-            
-            Stream stream = File.Open(fileName2, FileMode.Open);
-            
-            var aaa = await ipfs.FileSystem.AddAsync(stream, "ipfstest");
-            richTextBox1.Text = await ipfs.FileSystem.ReadAllTextAsync(aaa.Id.Hash.ToString());
-            
         }
-
-        /**
-        public class IpfsService
-        {
-
-            private string ipfsUrl;
-            public IpfsService(string ipfsUrl)
-            {
-                this.ipfsUrl = ipfsUrl;
-            }
-
-            public async Task<MerkleNode> AddFileAsync(string filePath)
-            {
-                var fileName = Path.GetFileName(filePath);
-                using (var fileStream = new FileStream(filePath, FileMode.Open))
-                {
-                    return await AddAsync(fileName, fileStream).ConfigureAwait(false);
-                }
-            }
-
-            public async Task<MerkleNode> AddAsync(string name, Stream stream)
-            {
-                
-                using (var ipfs = new IpfsClient())
-                {
-                    var inputStream = new IpfsStream(name, stream);
-
-                    var merkleNode = await ipfs.Add(inputStream).ConfigureAwait(false);
-                    var multiHash = ipfs.Pin.Add(merkleNode.Hash.ToString());
-                    return merkleNode;
-                }
-            }
-
-            public async Task<HttpContent> PinListAsync()
-            {
-                using (var ipfs = new IpfsClient(ipfsUrl))
-                {
-                    return await ipfs.Pin.Ls();
-                }
-            }
-        } */
     }
 }
